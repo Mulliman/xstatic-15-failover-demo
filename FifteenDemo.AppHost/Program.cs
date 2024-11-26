660356e9f86c78eea5b3b6cffedd1cd4b1767ff3
Sam Mullins <sammullins.co.uk@gmail.com>
2024-11-26 20:26:59 +0000

    missing lines

--- FifteenDemo.AppHost/Program.cs
+++ FifteenDemo.AppHost/Program.cs
@@ -5,7 +5,12 @@ var builder = DistributedApplication.CreateBuilder(args);
 var umbraco = builder.AddProject<Projects.FifteenDemo_Umbraco>("umbraco");
 var staticSite = builder.AddProject<Projects.FifteenDemo_StaticSite>("staticsite");
 
-builder.AddYarp("yarp")
+builder.AddAzureFunctionsProject<Projects.FifteenDemo_AzureFunctions>("azurefunctions")
+    .WithEnvironment("Cms.BaseUri", umbraco.GetEndpoint("https"))
+    .WithEnvironment("Cms.ClientId", "umbraco-back-office-demo")
+    .WithEnvironment("Cms.ClientSecret", "1234567890");
+
+var yarp = builder.AddYarp("yarp")
     .WithHttpEndpoint(9999)
     .WithReference(umbraco)
     .WithReference(staticSite)
